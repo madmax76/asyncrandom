ad81f21804173c64dc6f70bf149cf31bfdc60ff7
Yavor Paunov <contact@yavorpaunov.com>
2017-03-19 14:48:58 +0100

    Change README format to rst. Auto deploy to testpypi using travis.

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 from setuptools import setup
 
+readme_file = open('README.rst')
+long_description = readme_file.read()
+
 setup(name="asyncrandom",
-      description="Async random int generator.",
+      description="Async random number generator.",
       version="0.0.1",
       author="Yavor Paunov",
+      long_description=long_description,
       author_email='contact@yavorpaunov.com',
+      license='MIT',
       entry_points={
           "console_scripts": ["asyncrandom=asyncrandom:main"]
       },
